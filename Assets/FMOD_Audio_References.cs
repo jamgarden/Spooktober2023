cff711178d0e6a932194b4a070dbab40b99ae3dc
Matthew <Music.matthew.wood@gmail.com>
2023-09-20 14:08:56 -0400

    FMOD Integration

---
+++ Assets/FMOD_Audio_References.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMOD_Audio_References : MonoBehaviour
+{
+    [SerializeField] private StudioEventEmitter soundEffect1;
+    [SerializeField] private StudioEventEmitter voiceLines;
+}
