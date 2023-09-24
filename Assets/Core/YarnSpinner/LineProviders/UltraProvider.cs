4f40f5f85a5f08856c35f8b6fbfbba5b5db33287
Zach Jones <zacharytjones1@gmail.com>
2023-09-24 13:33:12 -0700

    Added functionality to the UltraProvider, can add commands for yarnspinner and FMOD integration

--- Assets/Core/YarnSpinner/LineProviders/UltraProvider.cs
+++ Assets/Core/YarnSpinner/LineProviders/UltraProvider.cs
@@ -3,7 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
-public class UltraProvider : TextLineProvider
+
+namespace Yarn.Unity
 {
-    
+
+    public class UltraProvider : LineProviderBehaviour
+    {
+
+        [Language]
+        public string textLanguageCode = System.Globalization.CultureInfo.CurrentCulture.Name;
+        public override LocalizedLine GetLocalizedLine(Line line)
+        {
+            var text = YarnProject.GetLocalization(textLanguageCode).GetLocalizedString(line.ID);
+
+            return new LocalizedLine()
+            {
+                TextID = line.ID,
+                RawText = text,
+                Substitutions = line.Substitutions,
+                Metadata = YarnProject.lineMetadata.GetMetadata(line.ID),
+            };
+        }
+
+        public override string LocaleCode => textLanguageCode;
+    }
+
 }
