87fc3fa6c309138c9f28a169dc18d74795f56cf7
Matthew <Music.matthew.wood@gmail.com>
2023-09-25 16:30:47 -0400

    Line VO Switch working

--- Assets/Core/YarnSpinner/LineProviders/UltraProvider.cs
+++ Assets/Core/YarnSpinner/LineProviders/UltraProvider.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
-
+using FMODUnity;
+using FMOD;
+using FMOD.Studio;
 
 namespace Yarn.Unity
 {
-
+    
     public class UltraProvider : LineProviderBehaviour
     {
+        [SerializeField] private StudioEventEmitter emitter;
+        [SerializeField] private EventReference eventReference;
 
+        private string currentID;
         [Language]
         public string textLanguageCode = System.Globalization.CultureInfo.CurrentCulture.Name;
         public override LocalizedLine GetLocalizedLine(Line line)
         {
             var text = YarnProject.GetLocalization(textLanguageCode).GetLocalizedString(line.ID);
+            currentID = line.ID;
+            emitter.Stop();
+            string clean = line.ID.Remove(4, 1);
+            currentID = clean;
+            UnityEngine.Debug.Log(clean);
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByNameWithLabel("YarnLines", clean);
+            emitter.Play(); 
 
             return new LocalizedLine()
             {
