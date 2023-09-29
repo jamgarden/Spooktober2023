32406e2053466bc4b02bc06b42e3b0d519cd453a
Matthew <Music.matthew.wood@gmail.com>
2023-09-29 18:55:39 -0400

    FMOD Works!!

--- Assets/Core/YarnSpinner/LineProviders/UltraProvider.cs
+++ Assets/Core/YarnSpinner/LineProviders/UltraProvider.cs
@@ -8,25 +8,40 @@ using FMOD.Studio;
 
 namespace Yarn.Unity
 {
-    
+
     public class UltraProvider : LineProviderBehaviour
     {
         [SerializeField] private StudioEventEmitter emitter;
         [SerializeField] private EventReference eventReference;
+        [SerializeField] private PARAMETER_ID yarnLines;
+        [SerializeField] private StudioEventEmitter stopVO;
+        private FMOD.Studio.EventInstance otherInstance;
+
+        
 
         private string currentID;
         [Language]
         public string textLanguageCode = System.Globalization.CultureInfo.CurrentCulture.Name;
         public override LocalizedLine GetLocalizedLine(Line line)
         {
+            
+            // throwAway is there to hold the original instance
+            FMOD.Studio.EventInstance throwAway = otherInstance;
+            throwAway.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            otherInstance = RuntimeManager.CreateInstance(eventReference);
             var text = YarnProject.GetLocalization(textLanguageCode).GetLocalizedString(line.ID);
             currentID = line.ID;
-            emitter.Stop();
+            
             string clean = line.ID.Remove(4, 1);
             currentID = clean;
             UnityEngine.Debug.Log(clean);
-            FMODUnity.RuntimeManager.StudioSystem.setParameterByNameWithLabel("YarnLines", clean);
-            emitter.Play(); 
+
+            FMOD.Studio.EventDescription eventDescription;
+            FMODUnity.RuntimeManager.StudioSystem.getEvent("event:/VoiceLines/YarnVoiceLinePlayer", out eventDescription);
+            
+
+            otherInstance.setParameterByNameWithLabel("YarnLines", clean, false);
+            otherInstance.start();
 
             return new LocalizedLine()
             {
